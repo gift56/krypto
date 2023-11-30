bbcf5685144c46380488b8adcb903001a6abb274
gift56 <efeasiughu@gmail.com>
2023-11-30 17:13:42 -0500

    working on constant data

---
+++ src/constant.js
@@ -0,0 +1,18 @@
+export const heroCoins = [
+  {
+    img: "/img/bitcoinsmallCoin.png",
+    text: "Bitcoin",
+  },
+  {
+    img: "/img/litesmallCoin.png",
+    text: "Litecoin",
+  },
+  {
+    img: "/img/chainlinkCoin.png",
+    text: "Chainlink",
+  },
+  {
+    img: "/img/siaCoin.png",
+    text: "Siacoin",
+  },
+];
