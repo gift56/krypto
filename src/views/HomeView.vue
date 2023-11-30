83151791bf5a094f631f117ee49f7a89135a5a6c
gift56 <efeasiughu@gmail.com>
2023-11-30 17:46:15 -0500

    working on header and footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+</script>
 
 <template>
+  <Header />
   <main>Hello world</main>
+  <Footer />
 </template>
