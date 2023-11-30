536956124978f449da424fca94308bbd0bbdfd10
gift56 <efeasiughu@gmail.com>
2023-11-30 07:30:03 -0500

    added variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EF443B",
+        bodybg: "#160E33",
+      },
+      boxShadow: {
+        stepShad: "0px 0px 40px 0px rgba(239, 68, 59, 0.40)",
+      },
+    },
   },
   plugins: [],
 };
