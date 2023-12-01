db52b600205b16031c7a50cc3fb7e88a4e8b6153
gift56 <efeasiughu@gmail.com>
2023-12-01 08:59:12 -0500

    working on advance section

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>Advance</section>
+  <section class="w-full bg-herobg bg-no-repeat bg-center md:bg-cover transition-all duration-200">Advance</section>
 </template>
 
 <script setup></script>
