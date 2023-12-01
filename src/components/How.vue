ff27487741ee6478fffb783ee2e33db71f72d1de
gift56 <efeasiughu@gmail.com>
2023-12-01 13:53:03 -0500

    perfecting the project

--- src/components/How.vue
+++ src/components/How.vue
@@ -5,10 +5,30 @@
         class="w-full py-20 flex flex-col items-center justify-center gap-8 md:gap-12"
       >
         <div class="w-full flex flex-col items-center justify-center gap-4">
-          <h2 class="text-2xl font-bold text-center md:text-5xl lg:text-6xl">
+          <h2
+            v-motion
+            :initial="{ opacity: 0, y: 50 }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 100,
+              },
+            }"
+            class="text-2xl font-bold text-center md:text-5xl lg:text-6xl"
+          >
             How to Trade With Krypto
           </h2>
           <p
+            v-motion
+            :initial="{ opacity: 0, y: 50 }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 100,
+              },
+            }"
             class="text-lg font-normal text-center md:text-xl text-white/50 lg:text-2xl"
           >
             Tincidunt id nibh orci nibh just nulla elementum, sed vel.
@@ -18,7 +38,21 @@
           class="w-full flex flex-col md:flex-row items-center justify-center gap-6"
         >
           <div class="w-full md:flex-1">
-            <img src="/img/howImage.png" alt="how" class="lg:h-[500px]" />
+            <img
+              v-motion
+              :initial="{ opacity: 0, scale: 0.5 }"
+              :visible="{
+                opacity: 1,
+                scale: 1,
+                transition: {
+                  delay: 100,
+                  duration: 500,
+                },
+              }"
+              src="/img/howImage.png"
+              alt="how"
+              class="lg:h-[500px]"
+            />
           </div>
           <div
             class="w-full md:flex-1 flex flex-col items-center md:items-start justify-start gap-5"
