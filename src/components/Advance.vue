8907ecc7ad1ab8f683f319106ac5c1eb11bdfac9
gift56 <efeasiughu@gmail.com>
2023-12-01 09:01:55 -0500

    working on advance section

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -7,10 +7,7 @@
         <h1 class="text-2xl font-bold text-center md:text-5xl lg:text-6xl">
           Most advanced crypto trading platfrom ever!
         </h1>
-        <div class="flex items-center justify-center gap-3 md:gap-4">
-         
-        </div>
-        
+        <div class="flex items-center justify-center gap-3 md:gap-4"></div>
       </div>
     </div>
   </section>
