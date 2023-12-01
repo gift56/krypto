c7bf989359f793a7ce3b718a0c1ff3b73f1b5d4d
gift56 <efeasiughu@gmail.com>
2023-12-01 05:54:30 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,8 +3,14 @@
     class="w-full md:h-screen bg-herobg bg-no-repeat bg-cover bg-fixed transition-all duration-200"
   >
     <div class="container h-full flex items-center justify-center relative">
-      <div class="w-full flex items-center justify-center flex-col gap-8 py-14">
-
+      <div
+        class="w-full flex items-center justify-center flex-col gap-8 py-14 md:py-0"
+      >
+        <h1
+          class="text-3xl font-bold text-center md:text-5xl lg:text-7xl xl:text-[80px] xl:leading-[normal] xl:max-w-[730px]"
+        >
+          Next Generation Crypto Trading
+        </h1>
       </div>
     </div>
   </section>
