c63d0af60e8baa37a2bdb5e91715ad671facada3
gift56 <efeasiughu@gmail.com>
2023-12-01 08:32:02 -0500

    working on feature section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import Header from "../components/Header.vue";
 import Hero from "../components/Hero.vue";
 import How from "../components/How.vue";
 import Available from "../components/Available.vue";
+import Features from "../components/Features.vue";
 import Footer from "../components/Footer.vue";
 </script>
 
@@ -12,6 +13,7 @@ import Footer from "../components/Footer.vue";
     <Hero />
     <How />
     <Available />
+    <Features />
   </main>
   <Footer />
 </template>
