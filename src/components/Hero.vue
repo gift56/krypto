30fee6c377aa47b2e2d51a5d6ac1a9b2b6a2afa8
gift56 <efeasiughu@gmail.com>
2023-12-01 05:49:45 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="w-full relative md:h-screen bg-herobg bg-no-repeat bg-cover"></section>
+  <section
+    class="w-full relative md:h-screen bg-herobg bg-no-repeat bg-cover transition-all duration-200"
+  ></section>
 </template>
 
 <script setup></script>
