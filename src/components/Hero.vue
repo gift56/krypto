435bd01355f758c43c02239e13d99c2ef2d9b1a6
gift56 <efeasiughu@gmail.com>
2023-12-01 05:49:54 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="w-full relative md:h-screen bg-herobg bg-no-repeat bg-cover transition-all duration-200"
+    class="w-full relative md:h-screen bg-herobg bg-no-repeat bg-cover bg-fixed transition-all duration-200"
   ></section>
 </template>
 
