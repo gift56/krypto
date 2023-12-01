6ad7116b4f3feae8e2a4920a668dd60ff6200d1e
gift56 <efeasiughu@gmail.com>
2023-12-01 06:40:58 -0500

    working on how section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
   <section
-    class="w-full md:h-screen bg-herobg bg-no-repeat bg-center md:bg-cover transition-all duration-200"
+    class="w-full bg-herobg bg-no-repeat bg-center md:bg-cover transition-all duration-200"
   >
     <div class="container h-full flex items-center justify-center relative">
       <div
-        class="w-full flex items-center justify-center flex-col gap-8 py-14 md:py-0"
+        class="w-full flex items-center justify-center flex-col gap-8 py-14"
       >
         <h1
           class="text-3xl font-bold text-center md:text-5xl lg:text-7xl xl:text-[80px] xl:leading-[normal] xl:max-w-[730px]"
