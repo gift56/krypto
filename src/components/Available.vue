63967b2a90c4c69c36838b49de7c752d42306136
gift56 <efeasiughu@gmail.com>
2023-12-01 07:29:28 -0500

    working on next section

--- src/components/Available.vue
+++ src/components/Available.vue
@@ -4,8 +4,17 @@
       <div
         class="w-full py-20 flex flex-col items-center justify-center gap-8 md:gap-12"
       >
-    
-    </div>
+        <div class="w-full flex flex-col items-center justify-center gap-4">
+          <h2 class="text-2xl font-bold text-center md:text-5xl lg:text-6xl">
+            Available Cryptos
+          </h2>
+          <p
+            class="text-lg font-normal text-center md:text-xl text-white/50 lg:text-2xl"
+          >
+            Tincidunt id nibh orci nibh just nulla elementum, sed vel.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
