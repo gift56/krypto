0e4625cc611355e8091e3547b64dcca34d4c976b
gift56 <efeasiughu@gmail.com>
2023-12-01 14:19:46 -0500

    done perfecting the project

--- src/components/Crypto.vue
+++ src/components/Crypto.vue
@@ -94,6 +94,7 @@
           </div>
         </div>
         <button
+        
           type="button"
           class="rounded-full w-fit px-3 md:px-6 py-4 outline-none border-2 border-primary text-white bg-[#160e3380] text-sm sm:text-base font-bold uppercase hover:bg-primary/60 transition-all duration-300"
         >
