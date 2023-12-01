e868fc7fd581da627101908fd4da47c5b5126858
gift56 <efeasiughu@gmail.com>
2023-12-01 09:00:25 -0500

    working on advance section

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -2,10 +2,8 @@
   <section
     class="w-full bg-advancebg bg-no-repeat bg-center md:bg-cover md:h-screen"
   >
-  <div class="container h-full flex items-center justify-center relative">
-      <div
-        class="w-full flex items-center justify-center flex-col gap-8 py-14"
-      >
+    <div class="container h-full flex items-center justify-center relative">
+      <div class="w-full flex items-center justify-center flex-col gap-8 py-14">
         <h1
           class="text-3xl font-bold text-center md:text-5xl lg:text-7xl xl:text-[80px] xl:leading-[normal] xl:max-w-[730px]"
         >
@@ -29,7 +27,9 @@
             Trade crypto
           </button>
         </div>
-        <div class="w-full flex flex-col items-center justify-center gap-3 pb-6">
+        <div
+          class="w-full flex flex-col items-center justify-center gap-3 pb-6"
+        >
           <h4 class="text-lg font-medium text-center uppercase">We accept</h4>
           <div
             class="w-full flex flex-wrap items-center justify-center md:flex-nowrap gap-6"
