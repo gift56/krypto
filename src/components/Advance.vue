56b0d8e81a306dc3cf082b8ec19b2874ba2545a1
gift56 <efeasiughu@gmail.com>
2023-12-01 09:00:00 -0500

    working on advance section

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="w-full bg-advancebg bg-no-repeat bg-center md:bg-cover md:"
+    class="w-full bg-advancebg bg-no-repeat bg-center md:bg-cover md:h-screen"
   >
     Advance
   </section>
