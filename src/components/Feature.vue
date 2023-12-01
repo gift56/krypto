f49d3fcbcec1bd0329d6426a4537e9a38ff1d2b4
gift56 <efeasiughu@gmail.com>
2023-12-01 08:35:21 -0500

    working on feature section

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="w-full relative">
-    <div class="container"></div>
+    <div class="container">
+      <div class="w-full py-20 flex flex-col items-center justify-center gap-8 md:gap-12"></div>
+    </div>
   </section>
 </template>
 
