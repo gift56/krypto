0dceb076730cecc10b2385a308c425b94053249b
gift56 <efeasiughu@gmail.com>
2023-12-01 09:28:36 -0500

    working on crypto section

--- src/components/Crypto.vue
+++ src/components/Crypto.vue
@@ -23,15 +23,9 @@
             class="w-full flex flex-col items-start justify-start gap-3"
           >
             <div :key="index" class="flex items-center justify-start gap-5">
-              <img :src="feature.icon" :alt="feature.title" class="" />
-              <h2 class="text-xl font-medium md:text-2xl lg:text-3xl">
-                {{ feature.title }}
-              </h2>
+              
             </div>
-            <p class="text-lg md:text-2xl font-normal text-white/50">
-              Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et
-              ornare risus.
-            </p>
+           
           </div>
         </div>
       </div>
