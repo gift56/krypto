357bb9066a12529c1eb434dd2d9cafea9ffb94fe
gift56 <efeasiughu@gmail.com>
2023-12-01 09:11:15 -0500

    working on testimony section

--- src/components/Testimony.vue
+++ src/components/Testimony.vue
@@ -41,10 +41,9 @@
             <swiper-slide v-for="(item, index) in testimonyData">
               <div
                 :key="index"
-                class="w-full flex flex-col items-center justify-center px-2 py-4 border border-primary shadow-stepShad hover:shadow-stephoverShad transition-all duration-300 rounded-xl overflow-hidden"
+                class="w-full flex flex-col items-center justify-center px-2 py-4 border border-primary shadow-stepShad hover:shadow-stephoverShad transition-all duration-300 rounded-xl h-[450px] overflow-hidden"
               >
                 <img :src="item.headImage" :alt="item.title" class="w-full" />
-                
               </div>
             </swiper-slide>
           </swiper>
