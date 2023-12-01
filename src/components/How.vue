33c3abb8e34f5cda47b998885e45df41ef1d12aa
gift56 <efeasiughu@gmail.com>
2023-12-01 13:59:02 -0500

    perfecting the project

--- src/components/How.vue
+++ src/components/How.vue
@@ -4,7 +4,7 @@
       <img
         v-motion
         :initial="{ opacity: 0, scale: 0.5 }"
-        :enter="{
+        :visible="{
           opacity: 1,
           scale: 1,
           transition: {
@@ -15,6 +15,21 @@
         src="/img/etheriumCoin.png"
         alt="etheriumCoin"
         class="absolute top-0 left-0 w-14 h-14 md:w-32 md:h-32"
+      />
+      <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0.5 }"
+        :visible="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 200,
+            duration: 500,
+          },
+        }"
+        src="/img/etheriumCoin.png"
+        alt="etheriumCoin"
+        class="absolute top-[90%] right-0 w-14 h-14 md:w-32 md:h-32"
       />
       <div
         class="w-full py-20 flex flex-col items-center justify-center gap-8 md:gap-12"
