02e434a0d59858004a7926cf7d7d6a7956b7a8b8
gift56 <efeasiughu@gmail.com>
2023-12-01 05:12:18 -0500

    working on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,102 @@
 <template>
-  <header>Header</header>
+  <header class="w-full bg-transparent">
+    <div class="container relative z-40">
+      <nav class="w-full flex items-center justify-between gap-6 py-6">
+        <RouterLink to="/">
+          <h2 class="select-none text-center text-2xl font-bold text-primary">
+            Krypto
+          </h2>
+        </RouterLink>
+        <nav class="hidden lg:flex items-center justify-start gap-6">
+          <RouterLink
+            v-for="link in navLinks"
+            :key="link.text"
+            :to="link.href"
+            class="text-base font-normal uppercase"
+            >{{ link.text }}</RouterLink
+          >
+        </nav>
+        <div class="hidden lg:flex items-center justify-start gap-6"></div>
+        <span @click="toggleMenu" v-if="!isMenuOpen" class="lg:hidden text-2xl">
+          <i class="fa-solid fa-bars"></i>
+        </span>
+      </nav>
+    </div>
+    <div
+      class="lg:hidden flex flex-col h-screen bg-black/20 gap-7 absolute top-0 w-full z-40 transition-all duration-300"
+      :class="{ 'left-[0]': isMenuOpen, '-left-[150%]': !isMenuOpen }"
+    >
+      <nav
+        ref="menuRef"
+        className="flex flex-col h-full bg-bodybg gap-5 p-4 w-[250px]"
+      >
+        <div className="w-full flex items-center justify-end">
+          <span @click="toggleMenu" className="text-2xl">
+            <i class="fa-solid fa-xmark"></i>
+          </span>
+        </div>
+        <RouterLink
+          v-for="link in navLinks"
+          :key="link.text"
+          :to="link.href"
+          @click="toggleMenu"
+          class="text-base font-normal uppercase"
+          >{{ link.text }}</RouterLink
+        >
+      </nav>
+    </div>
+  </header>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const isMenuOpen = ref(false);
+const menuRef = ref(null);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const closeMenuOnClickOutside = (event) => {
+  if (
+    isMenuOpen.value &&
+    menuRef.value &&
+    !menuRef.value.contains(event.target)
+  ) {
+    isMenuOpen.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("mousedown", closeMenuOnClickOutside);
+});
+
+// Clean up the event listener when the component is unmounted
+onUnmounted(() => {
+  document.removeEventListener("mousedown", closeMenuOnClickOutside);
+});
+
+const navLinks = [
+  {
+    href: "#how",
+    text: "How it works",
+  },
+  {
+    href: "#crypto",
+    text: "Cryptos",
+  },
+  {
+    href: "#features",
+    text: "Features",
+  },
+  {
+    href: "#testimonial",
+    text: "Testimonial",
+  },
+  {
+    href: "#university",
+    text: "University",
+  },
+];
 </script>
