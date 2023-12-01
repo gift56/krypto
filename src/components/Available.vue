984725bf69f4b6a0752cb73fa45c23f9c99c757d
gift56 <efeasiughu@gmail.com>
2023-12-01 07:28:58 -0500

    working on next section

--- src/components/Available.vue
+++ src/components/Available.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="w-full">
     <div class="container relative">
-      
+      <div
+        class="w-full py-20 flex flex-col items-center justify-center gap-8 md:gap-12"
+      >
+    
+    </div>
     </div>
   </section>
 </template>
