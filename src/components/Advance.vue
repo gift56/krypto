4f01f65a9f2ef69498086a6bfb6052de8c580474
gift56 <efeasiughu@gmail.com>
2023-12-01 14:08:56 -0500

    perfecting the project

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -4,15 +4,42 @@
   >
     <div class="container h-full flex items-center justify-center relative">
       <div class="w-full flex items-center justify-center flex-col gap-8 py-14">
-        <h1 class="text-2xl font-bold text-center md:text-5xl lg:text-6xl">
+        <h1
+          v-motion
+          :initial="{ opacity: 0, y: 50 }"
+          :visible="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: 100,
+            },
+          }"
+          class="text-2xl font-bold text-center md:text-5xl lg:text-6xl"
+        >
           Most advanced crypto trading platfrom ever!
         </h1>
         <div class="flex items-center justify-center gap-3 md:gap-4">
           <RouterLink to="/"
-            ><img src="/img/googlePlay.png" alt="googleplay"
+            ><img v-motion
+                :initial="{ opacity: 0, scale: 0 }"
+                :visible="{
+                  opacity: 1,
+                  scale: 1,
+                  transition: {
+                    delay: 100 * index,
+                  },
+                }" src="/img/googlePlay.png" alt="googleplay"
           /></RouterLink>
           <RouterLink to="/"
-            ><img src="/img/appleStore.png" alt="applestore"
+            ><img v-motion
+                :initial="{ opacity: 0, scale: 0 }"
+                :visible="{
+                  opacity: 1,
+                  scale: 1,
+                  transition: {
+                    delay: 100 * index,
+                  },
+                }"src="/img/appleStore.png" alt="applestore"
           /></RouterLink>
         </div>
       </div>
