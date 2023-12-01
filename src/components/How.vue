908ef87c7e339d167627544682415dbe4512e62e
gift56 <efeasiughu@gmail.com>
2023-12-01 06:39:57 -0500

    working on how section

--- src/components/How.vue
+++ src/components/How.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="w-full">
     <div class="container relative">
-      <div class="w-full py-14 flex flex-col items-center justify-center gap-8">
+      <div class="w-full py-20 flex flex-col items-center justify-center gap-8">
         <div class="w-full flex flex-col items-center justify-center gap-3">
           <h2 class="text-2xl font-bold text-center md:text-5xl lg:text-6xl">
             How to Trade With Krypto
