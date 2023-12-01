bb7d1c425b922e2f7f3904e79a17234e4585c47a
gift56 <efeasiughu@gmail.com>
2023-12-01 09:03:07 -0500

    working on advance section

--- src/components/Advance.vue
+++ src/components/Advance.vue
@@ -8,7 +8,7 @@
           Most advanced crypto trading platfrom ever!
         </h1>
         <div class="flex items-center justify-center gap-3 md:gap-4">
-          <RouterLink to="/"><img src="/img/google" alt=""></RouterLink>
+          <RouterLink to="/"><img src="/img/googlePlay.png" alt=""></RouterLink>
         </div>
       </div>
     </div>
