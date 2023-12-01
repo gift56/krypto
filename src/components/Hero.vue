c028d5753b4a11554f168327380d04ac63686515
gift56 <efeasiughu@gmail.com>
2023-12-01 05:52:24 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,8 +2,10 @@
   <section
     class="w-full md:h-screen bg-herobg bg-no-repeat bg-cover bg-fixed transition-all duration-200"
   >
-    <div class="container h-full flex items-center justify-center gap-6 relative">
-      <div></div>
+    <div class="container h-full flex items-center justify-center relative">
+      <div class="w-full flex items-center justify-center flex-col gap-8 py-14">
+
+      </div>
     </div>
   </section>
 </template>
