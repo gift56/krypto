a40f56d97535a3319afdce1874be705fe0200a9b
gift56 <efeasiughu@gmail.com>
2023-12-01 09:09:10 -0500

    working on testimony section

--- src/components/Testimony.vue
+++ src/components/Testimony.vue
@@ -44,30 +44,7 @@
                 class="w-full flex flex-col items-start justify-start rounded-xl overflow-hidden"
               >
                 <img :src="item.headImage" :alt="item.title" class="w-full" />
-                <div
-                  class="bg-white w-full flex flex-col items-start justify-start gap-10 p-3"
-                >
-                  <div>
-                    <h3 class="text-xl font-medium text-bodybg md:text-2xl">
-                      {{ item.title }}
-                    </h3>
-                    <p class="text-base font-normal md:text-lg text-bodybg/50">
-                      Tincidunt id nibh orci nibh justo. Purus et turpis nulla
-                      elementum, sed vel.
-                    </p>
-                  </div>
-                  <div class="w-full flex items-center justify-between gap-5">
-                    <h2 class="text-2xl font-bold text-primary uppercase">
-                      {{ item.price }}
-                    </h2>
-                    <button
-                      type="button"
-                      class="outline-none w-fit px-2 py-2 rounded-full bg-primary border border-primary hover:bg-primary/60 transition-all duration-300 text-base font-bold"
-                    >
-                      Trade Now
-                    </button>
-                  </div>
-                </div>
+                
               </div>
             </swiper-slide>
           </swiper>
