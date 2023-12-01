0e2bb52cdd2b2151a1f5a48590977b5869dfcbc1
gift56 <efeasiughu@gmail.com>
2023-12-01 09:46:00 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,5 +3,12 @@
 </template>
 
 <script setup>
-
+const links = ["About", "Career", "Press", "News", "Merch"];
+const termlinks = [
+  "CoinFlip Privacy Policy",
+  "CoinFlip Biometrics Privacy Policy",
+  "CoinFlip Financial Privacy Notice",
+  "CoinFlip Terms of Service",
+  "CoinFlip Trade Desk Terms of Service",
+];
 </script>
